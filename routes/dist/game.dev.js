cd6a6348c9649eab3176f9474e7849b0bf1e09ec
FarruxDEV <farruxbeknozimov@gmail.com>
2023-01-18 23:42:09 +0500

    Fix Input Bug

---
+++ routes/dist/game.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = require("express");
+
+var _auth = _interopRequireDefault(require("../middleware/auth.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var router = (0, _express.Router)();
+router.get("/games", _auth["default"], function (req, res) {
+  res.render("games", {
+    isGames: true,
+    user: req.user
+  });
+});
+router.get("/games/tictactoe", _auth["default"], function (req, res) {
+  res.render("games/tictactoe", {
+    isGames: true,
+    user: req.user
+  });
+});
+var _default = router;
+exports["default"] = _default;
--- routes/game.js
+++ routes/game.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 import authMiddleware from "../middleware/auth.js";
 const router = Router();
 
-// Import Models
-import User from "../models/User.js";
-import Game from "../models/Game.js";
-
 router.get("/games", authMiddleware, (req, res) => {
 	res.render("games", {
+		isGames: true,
+		user: req.user,
+	});
+});
+
+router.get("/games/tictactoe", authMiddleware, (req, res) => {
+	res.render("games/tictactoe", {
 		isGames: true,
 		user: req.user,
 	});
